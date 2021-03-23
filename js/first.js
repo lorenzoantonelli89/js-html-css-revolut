e568b3419754cb355a3b9923f998236746154eec
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-23 15:31:30 +0100

    inserito uscita al click, manca uscita quando vai su altro link menu

--- js/first.js
+++ js/first.js
@@ -1,14 +1,17 @@
-console.log('hello');
-console.log($);
+
 function menuHover() {
     $('.menu').hover(function () {
         $(this).find('.menu-hide').show();
+    }, function () {
+        // $('.menu').hover(function () {
+        //     $(this).find('.menu-hide').show();
+        // });
+        $('body').click(function () {
+            $('body').find('.menu-hide').hide();
+        });
     });
 
-    $('body').click(function () {
-        $('body').find('.menu-hide').hide();
-    });
-   
+    
     
 }
 
