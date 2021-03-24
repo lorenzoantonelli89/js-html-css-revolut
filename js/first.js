fb47f64f39d3af03733a2cef53fab8206dd10a87
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-24 17:56:13 +0100

    aggiunto show div menu con click su li

--- js/first.js
+++ js/first.js
@@ -1,17 +1,25 @@
 
 function menuHover() {
-    $('.menu').hover(function () {
-        $('.menu-hide.active').hide();
+    $('.menu').mouseenter(function () {
+        $('.menu-hide.active').hide().removeClass("active");
         $(this).find('.menu-hide').show().addClass("active");
     });
     
-
-    $('body').click(function () {
-        $('body').find('.menu-hide').hide();
+    $('body').click(function test () {
+        $('body').find('.menu-hide').hide().removeClass("active");
     });
 
     $('.menu-hide').click(function (event) {
         event.stopPropagation();
+    });
+
+    $('.menu').click(function (event) {
+
+        if (!$('.menu-hide').hasClass('active')) {
+            $(this).find('.menu-hide').show().addClass("active");
+            event.stopPropagation();
+
+        }
     });
     
 }
